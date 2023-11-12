ff802f994accad344ea7b455faca1740e8c34fd5
wojcikkuba <wojcikjakub@int.pl>
2023-11-12 14:44:22 +0100

    Add pagination

--- frontend/src/views/CurrentRequests.js
+++ frontend/src/views/CurrentRequests.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 import {
     Card,
     CardBody,
@@ -8,21 +6,38 @@ import {
     CardTitle,
     Row,
     Col,
+    Button,
+    Pagination,
+    PaginationItem,
+    PaginationLink
 } from "reactstrap";
 
 import PanelHeader from "components/PanelHeader/PanelHeader.js";
 
 function CurrentRequests() {
     const [requests, setRequests] = useState([]);
-
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 4;
+    const token = localStorage.getItem('token');
     useEffect(() => {
-        // Tutaj możemy pobrać aktualne prośby z API i ustawić je w stanie requests
-        // Przykładowe zapytanie do API:
-        // fetch("http://localhost:5000/requests")
-        //   .then((response) => response.json())
-        //   .then((data) => setRequests(data))
-        //   .catch((error) => console.error("Błąd pobierania danych z API:", error));
-    }, []); // Użyj pustej tablicy zależności, aby useEffect wykonał się tylko raz po zamontowaniu komponentu
+        fetch("http://localhost:5000/request", {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            }
+        })
+           .then((response) => response.json())
+           .then((data) => setRequests(data))
+           .catch((error) => console.error("Błąd pobierania danych z API:", error));
+    }, []);
+
+    const indexOfLastRequest = currentPage * itemsPerPage;
+    const indexOfFirstRequest = indexOfLastRequest - itemsPerPage;
+    const currentRequests = requests.slice(indexOfFirstRequest, indexOfLastRequest);
+
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
 
     return (
         <>
@@ -35,19 +50,34 @@ function CurrentRequests() {
                                 <CardTitle tag="h4">Aktualne Prośby</CardTitle>
                             </CardHeader>
                             <CardBody>
-                                {requests.length > 0 ? (
-                                    <Carousel showArrows={true} showStatus={false} showThumbs={false}>
-                                        {requests.map((request) => (
-                                            <div key={request.id}>
-                                                <h3>{request.subjectName}</h3>
-                                                <p>{request.description}</p>
-                                                {/* Dodaj więcej informacji o prośbie, jeśli jest to konieczne */}
-                                            </div>
+                                {currentRequests.length > 0 ? (
+                                    <Row>
+                                        {currentRequests.map((request) => (
+                                            <Col key={request.id} md={6}>
+                                                <Card >
+                                                    <CardBody>
+                                                        <p><b>Użytkownik:</b> {request.user.firstName} {request.user.lastName}</p>
+                                                        <p><b>Komentarz:</b> {request.comment}</p>
+                                                        <p><b>Przedmiot:</b> {request.subject.id}</p>
+                                                        <p><b>Data:</b> {request.date}</p>
+                                                        <Button color="primary" className="mb-15">Zgłoś się</Button>
+                                                    </CardBody>
+                                                </Card>
+                                            </Col>
                                         ))}
-                                    </Carousel>
+                                    </Row>
                                 ) : (
                                     <p className="text-info h3 text-center">Brak aktualnych próśb.</p>
                                 )}
+                                <Pagination>
+                                    {[...Array(Math.ceil(requests.length / itemsPerPage))].map((_, index) => (
+                                        <PaginationItem key={index} active={index + 1 === currentPage}>
+                                            <PaginationLink onClick={() => handlePageChange(index + 1)}>
+                                                {index + 1}
+                                            </PaginationLink>
+                                        </PaginationItem>
+                                    ))}
+                                </Pagination>
                             </CardBody>
                         </Card>
                     </Col>
