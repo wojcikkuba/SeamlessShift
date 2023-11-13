be632a029e48e9e878c9e3409a79535a43f00649
wojcikkuba <wojcikjakub@int.pl>
2023-11-14 00:46:38 +0100

    Add regex for forms validation

---
+++ frontend/src/utils/patterns.js
@@ -0,0 +1,6 @@
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const phonePattern = /^\d{9}$/;
+const firstNamePattern = /^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ-]+$/;
+const lastNamePattern = /^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ -]+$/;
+
+export { emailPattern, phonePattern, firstNamePattern, lastNamePattern };
