7c39e2b0e59198bee872ad27c96f7f8a0a56ef04
wojcikkuba <wojcikjakub@int.pl>
2023-12-05 20:23:24 +0100

    Change error alert color

--- frontend/src/views/RestorePassword.js
+++ frontend/src/views/RestorePassword.js
@@ -1,75 +1,76 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function RestorePassword() {
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        setMessage('');
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setMessage("");
 
-        try {
-            const response = await fetch('http://localhost:5000/restore-password', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ email }),
-            });
+    try {
+      const response = await fetch("http://localhost:5000/restore-password", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email }),
+      });
 
-            if (response.ok) {
-                setMessage('Hasło czasowe zostało wysłane');
-                setTimeout(() => {
-                    navigate('/login');
-                }, 3000);
-            } else {
-                setMessage('Wystąpił błąd. Spróbuj ponownie.');
-            }
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    };
+      if (response.ok) {
+        setMessage("Hasło czasowe zostało wysłane");
+        setTimeout(() => {
+          navigate("/login");
+        }, 3000);
+      } else {
+        setMessage("Wystąpił błąd. Spróbuj ponownie.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setMessage("Wystąpił błąd. Spróbuj ponownie.");
+    }
+  };
 
-    return (
-        <div className="container mt-5">
-            <div className="row justify-content-center">
-                <div className="col-md-6">
-                    <div className="card">
-                        <div className="card-header">
-                            <h1 className="text-center">Przywróć hasło</h1>
-                        </div>
-                        <div className="card-body">
-                            {message && (
-                                <div className="alert alert-success text-center" role="alert">
-                                    {message}
-                                </div>
-                            )}
-                            <form onSubmit={handleSubmit}>
-                                <div className="mb-3">
-                                    <label htmlFor="email" className="form-label">
-                                        E-mail
-                                    </label>
-                                    <input
-                                        className="form-control"
-                                        type="email"
-                                        id="email"
-                                        name="email"
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        required
-                                        autoFocus
-                                    />
-                                </div>
-                                <div className="text-center">
-                                    <button type="submit" className="btn btn-primary">
-                                        Przywróć hasło
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-header">
+              <h1 className="text-center">Przywróć hasło</h1>
             </div>
+            <div className="card-body">
+              {message && (
+                <div className="alert alert-danger text-center" role="alert">
+                  {message}
+                </div>
+              )}
+              <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                  <label htmlFor="email" className="form-label">
+                    E-mail
+                  </label>
+                  <input
+                    className="form-control"
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    autoFocus
+                  />
+                </div>
+                <div className="text-center">
+                  <button type="submit" className="btn btn-primary">
+                    Przywróć hasło
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
