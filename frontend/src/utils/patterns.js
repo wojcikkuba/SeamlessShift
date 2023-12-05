41dd2a544257ae85ee22407b8579bc4d4b944ca3
wojcikkuba <wojcikjakub@int.pl>
2023-12-05 20:05:07 +0100

    Add password regex

--- frontend/src/utils/patterns.js
+++ frontend/src/utils/patterns.js
@@ -2,5 +2,6 @@ const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const phonePattern = /^\d{9}$/;
 const firstNamePattern = /^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ-]+$/;
 const lastNamePattern = /^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ -]+$/;
+const passwordRegex = /^(?=.*[A-Z])(?=.*\d).{8,}$/;
 
-export { emailPattern, phonePattern, firstNamePattern, lastNamePattern };
+export { emailPattern, phonePattern, firstNamePattern, lastNamePattern, passwordRegex };
