17260d2c622f3f57d2bb17f093f874323a07639d
wojcikkuba <wojcikjakub@int.pl>
2023-12-05 20:04:45 +0100

    Set timeout to logout

--- frontend/src/services/AuthService.js
+++ frontend/src/services/AuthService.js
@@ -1,105 +1,110 @@
-import isTokenExpired from '../utils/isTokenExpired';
+import isTokenExpired from "../utils/isTokenExpired";
 
 class AuthService {
-    static isAuthenticated() {
-        const token = localStorage.getItem('token');
-        if (!token || isTokenExpired(token)) {
-            return false;
-        }
-        return true;
+  static isAuthenticated() {
+    const token = localStorage.getItem("token");
+    if (!token || isTokenExpired(token)) {
+      return false;
     }
+    return true;
+  }
 
-    static isAdmin() {
-        const user = localStorage.getItem('user');
-        if (user) {
-            const parsedUser = JSON.parse(user);
-            return parsedUser.role_id === 2;
-        }
-        return false;
+  static isAdmin() {
+    const user = localStorage.getItem("user");
+    if (user) {
+      const parsedUser = JSON.parse(user);
+      return parsedUser.role_id === 2;
     }
+    return false;
+  }
 
-    static isTokenExpired(token) {
-        const expirationDate = this.getTokenExpirationDate(token);
-        return expirationDate < new Date();
+  static isTokenExpired(token) {
+    const expirationDate = this.getTokenExpirationDate(token);
+    return expirationDate < new Date();
+  }
+
+  static isPasswordChangeRequired() {
+    const user = localStorage.getItem("user");
+    if (user) {
+      const parsedUser = JSON.parse(user);
+      return parsedUser.password_change_required === true;
     }
+    return false;
+  }
 
-    static isPasswordChangeRequired() {
-        const user = localStorage.getItem('user');
-        if (user) {
-            const parsedUser = JSON.parse(user);
-            return parsedUser.password_change_required === true;
-        }
-        return false;
-    }
+  static login(email, password) {
+    return fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    }).then(async (response) => {
+      if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("user", JSON.stringify(data.user));
+        return data.user;
+      } else {
+        throw new Error("Login failed");
+      }
+    });
+  }
 
-    static login(email, password) {
-        return fetch('http://localhost:5000/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email, password }),
-        }).then(async (response) => {
-            if (response.ok) {
-                const data = await response.json();
-                localStorage.setItem('token', data.token);
-                localStorage.setItem('user', JSON.stringify(data.user));
-                return data.user;
-            } else {
-                throw new Error('Login failed');
-            }
+  static logout() {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      fetch("http://localhost:5000/logout", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((response) => {
+          if (response.ok) {
+            console.log("Logged out successfully");
+          } else {
+            console.error("Logout failed");
+          }
+        })
+        .catch((error) => {
+          console.error("Error during logout:", error);
         });
     }
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+  }
 
-    static logout() {
-        const token = localStorage.getItem('token');
+  static changePassword(changePasswordData) {
+    const token = localStorage.getItem("token");
 
-        if (token) {
-            fetch('http://localhost:5000/logout', {
-                method: 'POST',
-                headers: {
-                    'Authorization': `Bearer ${token}`,
-                }
-            }).then(response => {
-                if (response.ok) {
-                    console.log('Logged out successfully');
-                } else {
-                    console.error('Logout failed');
-                }
-            }).catch(error => {
-                console.error('Error during logout:', error);
-            });
-        }
-        localStorage.removeItem('token');
-        localStorage.removeItem('user');
+    if (!token) {
+      return Promise.reject(new Error("Brak autoryzacji"));
     }
 
-    static changePassword(changePasswordData) {
-        const token = localStorage.getItem('token');
+    return fetch("http://localhost:5000/change-password", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(changePasswordData),
+    }).then(async (response) => {
+      if (response.ok) {
+        console.log("Hasło zostało pomyślnie zmienione");
 
-        if (!token) {
-            return Promise.reject(new Error('Brak autoryzacji'));
-        }
+        setTimeout(() => {
+          this.logout();
+          window.location.href = "/login";
+        }, 5000);
 
-        return fetch('http://localhost:5000/change-password', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-            },
-            body: JSON.stringify(changePasswordData),
-        }).then(async (response) => {
-            if (response.ok) {
-                console.log('Hasło zostało pomyślnie zmienione');
-                this.logout();
-                window.location.href = "/login";
-                return;
-            } else {
-                throw new Error('Zmiana hasła nie powiodła się');
-            }
-        });
-    }
-
+        return;
+      } else {
+        throw new Error("Zmiana hasła nie powiodła się");
+      }
+    });
+  }
 }
 
 export default AuthService;
--- frontend/src/views/ChangePassword.js
+++ frontend/src/views/ChangePassword.js
@@ -1,109 +1,125 @@
-import React, { useState } from 'react';
-import AuthService from '../services/AuthService';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import AuthService from "../services/AuthService";
+import { Link } from "react-router-dom";
 import {
-    Card,
-    CardBody,
-    CardHeader,
-    CardTitle,
-    Row,
-    Col,
-    Input,
-    FormGroup,
-    Button,
-    Spinner,
-} from 'reactstrap';
+  Card,
+  CardBody,
+  CardHeader,
+  CardTitle,
+  Row,
+  Col,
+  Input,
+  FormGroup,
+  Button,
+  Alert,
+} from "reactstrap";
+import { passwordRegex } from "utils/patterns";
 
 export default function ChangePassword() {
-    const [changePasswordError, setChangePasswordError] = useState(null);
+  const [changePasswordError, setChangePasswordError] = useState(null);
+  const [changePasswordSuccess, setChangePasswordSuccess] = useState(false);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        const currentPassword = data.get('currentPassword');
-        const newPassword = data.get('newPassword');
-        const confirmNewPassword = data.get('confirmNewPassword');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const currentPassword = data.get("currentPassword");
+    const newPassword = data.get("newPassword");
+    const confirmNewPassword = data.get("confirmNewPassword");
 
-        if (newPassword !== confirmNewPassword) {
-            setChangePasswordError('Nowe hasło i potwierdzenie hasła nie pasują do siebie.');
-            return;
-        }
+    if (!passwordRegex.test(newPassword)) {
+        setChangePasswordError("Hasło musi mieć co najmniej 8 znaków, jedną cyfrę i jedną dużą literę.");
+        return;
+    }
 
-        const currentUserEmail = JSON.parse(localStorage.getItem('user')).email;
+    if (newPassword !== confirmNewPassword) {
+      setChangePasswordError("Hasła różnią się.");
+      return;
+    }
 
-        const changePasswordData = {
-            email: currentUserEmail,
-            old_password: currentPassword,
-            new_password: newPassword,
-        };
+    const currentUserEmail = JSON.parse(localStorage.getItem("user")).email;
 
-        AuthService.changePassword(changePasswordData)
-            .then(() => {
-                console.log('Hasło zostało pomyślnie zmienione.');
-            })
-            .catch((error) => {
-                console.error('Change password error', error);
-                setChangePasswordError('Nieprawidłowe dane do zmiany hasła. Spróbuj ponownie.');
-            });
+    const changePasswordData = {
+      email: currentUserEmail,
+      old_password: currentPassword,
+      new_password: newPassword,
     };
 
-    const returnPath = `/${AuthService.isAdmin() ? 'admin' : 'user'}/user-page`;
+    AuthService.changePassword(changePasswordData)
+      .then(() => {
+        console.log("Hasło zostało pomyślnie zmienione. Za chwilę zostaniesz wylogowany.");
+        setChangePasswordSuccess(true);
+        setChangePasswordError(false);
+      })
+      .catch((error) => {
+        console.error("Change password error", error);
+        setChangePasswordError(
+          "Nieprawidłowe dane do zmiany hasła. Spróbuj ponownie."
+        );
+      });
+  };
 
-    return (
-        <div className="content">
-            <Row>
-                <Col xs={12} md={6} className="mx-auto">
-                    <Card className="card-login">
-                        <CardHeader className="text-center">
-                            <CardTitle tag="h4">Zmiana hasła</CardTitle>
-                        </CardHeader>
-                        <CardBody>
-                            {changePasswordError && (
-                                <div className="alert alert-danger text-center" role="alert">
-                                    {changePasswordError}
-                                </div>
-                            )}
-                            <form onSubmit={handleSubmit}>
-                                <FormGroup>
-                                    <Input
-                                        type="password"
-                                        id="currentPassword"
-                                        name="currentPassword"
-                                        placeholder="Obecne hasło (min 8 znakow)"
-                                        required
-                                    />
-                                </FormGroup>
-                                <FormGroup>
-                                    <Input
-                                        type="password"
-                                        id="newPassword"
-                                        name="newPassword"
-                                        placeholder="Nowe hasło"
-                                        required
-                                    />
-                                </FormGroup>
-                                <FormGroup>
-                                    <Input
-                                        type="password"
-                                        id="confirmNewPassword"
-                                        name="confirmNewPassword"
-                                        placeholder="Potwierdź nowe hasło"
-                                        required
-                                    />
-                                </FormGroup>
-                                <div className="text-center">
-                                    <Button type="submit" color="primary">
-                                        Zapisz zmiany
-                                    </Button>
-                                    <Link to={returnPath} className="btn btn-secondary ml-2">
-                                        Powrót
-                                    </Link>
-                                </div>
-                            </form>
-                        </CardBody>
-                    </Card>
-                </Col>
-            </Row>
-        </div>
-    );
+  const returnPath = `/${AuthService.isAdmin() ? "admin" : "user"}/user-page`;
+
+  return (
+    <div className="content">
+      <Row>
+        <Col xs={12} md={6} className="mx-auto">
+          <Card className="card-login">
+            <CardHeader className="text-center">
+              <CardTitle tag="h4">Zmiana hasła</CardTitle>
+            </CardHeader>
+            <CardBody>
+              {changePasswordError && (
+                <div className="alert alert-danger text-center" role="alert">
+                  {changePasswordError}
+                </div>
+              )}
+              {changePasswordSuccess && (
+                <Alert color="success">
+                  Hasło zostało pomyślnie zmienione! Za chwilę zostaniesz wylogowany.
+                </Alert>
+              )}
+              <form onSubmit={handleSubmit}>
+                <FormGroup>
+                  <Input
+                    type="password"
+                    id="currentPassword"
+                    name="currentPassword"
+                    placeholder="Obecne hasło (min 8 znakow)"
+                    required
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Input
+                    type="password"
+                    id="newPassword"
+                    name="newPassword"
+                    placeholder="Nowe hasło"
+                    required
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Input
+                    type="password"
+                    id="confirmNewPassword"
+                    name="confirmNewPassword"
+                    placeholder="Potwierdź nowe hasło"
+                    required
+                  />
+                </FormGroup>
+                <div className="text-center">
+                  <Button type="submit" color="primary">
+                    Zapisz zmiany
+                  </Button>
+                  <Link to={returnPath} className="btn btn-secondary ml-2">
+                    Powrót
+                  </Link>
+                </div>
+              </form>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
 }
